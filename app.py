21f9021f521d10bf371bc0a6621fd0b8d6dab22a
meredith <meredith@paom.com>
2023-04-18 18:58:20 -0400

    ready for Heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import gunicorn
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
    return render_template('index.html')
